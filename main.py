5c0f1678aba9819d53c3658c89d8d75cd745cc73
Simon Tas <simon.tas.st@gmail.com>
2018-11-16 16:28:45 +0100

    implemented functions for updating db

--- main.py
+++ main.py
@@ -13,6 +13,32 @@ db = mysql.connector.connect(
   passwd=config['MySQL']['Password'],
   database=config['MySQL']['Database'])
 
+def updateBike(id, longtitude, altitude, battery):
+    db.cursor().execute("""
+    UPDATE bikes_bike
+    SET longtitude = (?), altitude = (?), battery = (?)
+    WHERE {} = id;
+    """, (longtitude, altitude, battery))
+
+def updateBikeSecret(id, secret):
+    db.cursor().execute("""
+    UPDATE bikes_bike
+    SET secret = (?)
+    WHERE {} = id;
+    """, (secret,))
+
+def endContract(id, startD):
+    db.cursor().execute("""
+    UPDATE bikes_contract
+    SET startD = (?)
+    WHERE EXISTS (
+    SELECT bikes_bike.id
+    FROM bikes_bike, bikes_contract
+    WHERE bikes_bike.id = bikes_contract.bike_id
+    AND bikes_contract.time_end = NULL
+    );
+    """, (startD,))
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("test")
@@ -25,6 +51,7 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+    db.cursor().execute("")
 
 # Create the mqtt client
 client = mqtt.Client()
